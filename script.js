9fb0c87b7396215b83cf9769c8a7d43bf2424a21
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2020-08-31 00:59:36 -0300

    configuração do painel

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("#jogo").addEventListener("click",function(){
+document.querySelector(".master").classList.toggle("show-menu-jogos");
+});
+document.querySelector("#eventos").addEventListener("click",function(){
+document.querySelector(".master").classList.toggle("show-menu-eventos");
+});
